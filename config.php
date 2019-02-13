af8d44576e682fa80b06b5adc36320a5a45ad2ea
n13family <42956023+n13family@users.noreply.github.com>
2019-02-13 17:21:38 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+$key = "TRIAL";
+
+$addr = array(
+			"recommendationFlow" => true,
+			"lastName"			 => "",
+			"firstName"			 => "",
+			"addressLine1"		 => "",
+			"addressLine2"		 => "",
+			"stateOrProvince"	 => "",
+			"city"				 => "",
+			"postalCode"		 => "",
+			"country"			 => "",
+			"phoneCountryCode"	 => "",
+			"phoneNumber"		 => "",
+			"email"				 => "",
+			"emailConfirm"		 => "",
+			"makePrimary"		 => false,
+			"disableValidation"	 => false,
+			"pageType"			 => "ryp"
+		);
